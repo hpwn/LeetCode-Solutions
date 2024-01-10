b30befa50ada172ba1b07adeec4760da1274f3b1
Hayden Price <hwplivnlow@gmail.com>
2024-01-09 20:04:23 -0700

    88

---
+++ 88.merge-sorted-array.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=88 lang=typescript
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  let p1: number = m - 1;
+  let p2: number = n - 1;
+
+  let p: number = m + n - 1;
+
+  while (p1 >= 0 && p2 >= 0) {
+    if (nums1[p1] > nums2[p2]) {
+      nums1[p] = nums1[p1];
+      p1--;
+    } else {
+      nums1[p] = nums2[p2];
+      p2--;
+    }
+    p--;
+  }
+
+  while (p2 >= 0) {
+    nums1[p] = nums2[p2];
+    p2--;
+    p--;
+  }
+}
+
+// @lc code=end
