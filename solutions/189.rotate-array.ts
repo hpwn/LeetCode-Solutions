7bfba8c7cf90fd3bc62f19e5a6d492ef5ed089d8
Hayden Price <hwplivnlow@gmail.com>
2024-01-11 13:14:42 -0700

    create solutions folder

---
+++ solutions/189.rotate-array.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=189 lang=typescript
+ *
+ * [189] Rotate Array
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+    
+};
+// @lc code=end
+
