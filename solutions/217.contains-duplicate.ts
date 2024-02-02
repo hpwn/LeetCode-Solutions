5925ef98d8533e1c0ad680d2b8d233a98bd26ac8
Hayden Price <hwplivnlow@gmail.com>
2024-02-02 11:17:34 -0700

    217

---
+++ solutions/217.contains-duplicate.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=217 lang=typescript
+ *
+ * [217] Contains Duplicate
+ */
+
+// notes: depending on time/memory needs you can sort in place
+// this will reduce worst case time to O(nlogn)
+// but improve memory to O(1)
+
+// @lc code=start
+function containsDuplicate(nums: number[]): boolean {
+  const numSet = new Set<number>();
+  for (let num of nums) {
+    if (numSet.has(num)) {
+      return true;
+    } else {
+      numSet.add(num);
+    }
+  }
+  return false;
+}
+// @lc code=end
